de1ea65d52a6898f74c7aa4d865e2b22e06e5323
suhanigarg29 <96908018+suhanigarg29@users.noreply.github.com>
2024-03-16 13:17:53 +0530

    File Structure built & Router added

--- mining-library/src/components/Coursepage/Sem2/sem2.tsx
+++ mining-library/src/components/Coursepage/Sem2/sem2.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import style from './homepage.module.css'
-const Home = () => {
+import { Link } from "react-router-dom";
+import style from './sem2.module.css'
+
+const Sem2 = () => {
     return (
         <>
             <div className={style.sem2page}>
                 <h1 className={style.sem2Title}>Semester II</h1>
                 <div className={style.sem2Container}>
-                    <div className={style.coursebutton}><a className={style.sem2} href=''>MN-111</a></div>
-                    <div className={style.coursebutton}><a className={style.sem2} href=''>MN-112</a></div>
+                    <div className={style.coursebutton}><Link to="/mn111" style={{ textDecoration: 'none' }}>MN-111</Link></div>
+                    <div className={style.coursebutton}><Link to="/mn112" style={{ textDecoration: 'none' }}>MN-112</Link></div>
                 </div>
 
             </div>
@@ -15,4 +17,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Sem2
---
+++ mining-library/src/components/Courses/MN111/mn111.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import style from './mn111.module.css'
+
+const MN111 = () => {
+    return (
+        <>
+            <div className={style.mn111page}>
+                <h1 className={style.mn111Title}>MN-111</h1>
+                
+
+            </div>
+        </>
+    )
+}
+
+export default MN111
