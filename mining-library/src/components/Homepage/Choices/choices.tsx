63cae386fb6bbc82bb72c7bdfbbe486b116545a0
suhanigarg29 <96908018+suhanigarg29@users.noreply.github.com>
2024-04-29 02:58:18 +0530

    Homepage updated

--- mining-library/src/components/Homepage/Choices/choices.tsx
+++ mining-library/src/components/Homepage/Choices/choices.tsx
@@ -8,7 +8,7 @@ const Choices = () => {
             <div className={style.chooseSem}>
                 <h1 className={style.semTitle}>Choose Semester</h1>
                 <div className={style.semContainer}>
-                    <div className={style.sembutton}><Link to="/sem1" style={{ textDecoration: 'none' }}><div className={style.sem}>SEMESTER I</div></Link></div>
+                    {/* <div className={style.sembutton}><Link to="/sem1" style={{ textDecoration: 'none' }}><div className={style.sem}>SEMESTER I</div></Link></div> */}
                     <div className={style.sembutton}><Link to="/sem2" style={{ textDecoration: 'none' }}><div className={style.sem}>SEMESTER II</div></Link></div>
                     <div className={style.sembutton}><Link to="/sem3" style={{ textDecoration: 'none' }}><div className={style.sem}>SEMESTER III</div></Link></div>
                     <div className={style.sembutton}><Link to="/sem4" style={{ textDecoration: 'none' }}><div className={style.sem}>SEMESTER IV</div></Link></div>
